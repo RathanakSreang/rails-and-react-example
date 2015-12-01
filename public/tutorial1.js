64772216bff06c7ac35ee924532206abbe34ae4e
Rathanak Sreang <sreang.rathanak@framgia.com>
2015-12-01 11:06:41 +0700

    Rails and React

---
+++ public/tutorial1.js
@@ -0,0 +1,12 @@
+// var CommentBox = React.createClass({
+//  render: function() {
+//   return (
+//     <div className="commentBox">
+//       Hello, World! I am Learning React. here is comment Box.
+//     </div>
+//     );
+//  }
+// });
+// ReactDOM.render(
+// <CommentBox />, document.getElementById('content')
+// );
